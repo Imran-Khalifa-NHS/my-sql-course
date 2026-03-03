22314641a590cfd3f0ae9adbf8f0c3d51d0244d7
Imran-Khalifa-NHS <Imran.Khalifa5@nhs.net>
2026-03-03 11:47:46 +0000

    Add SQL script for large airport selection in Europe

---
+++ Recap Lesson.sql
@@ -0,0 +1,29 @@
+USE LearnSQL
+/**Intermediate SQL**/
+
+SELECT DISTINCT [type] 
+FROM airports;
+
+SELECT
+    A.name
+    ,A.latitude_deg
+    ,A.longitude_deg
+    ,A.iata_code
+    ,A.elevation_ft
+    ,ROUND(A.elevation_ft/3.28,1) AS Elevation_M
+    ,A.iso_country
+
+FROM
+    airports A
+
+WHERE 
+    A.TYPE= 'large_airport'
+    AND
+    A.continent = 'EU'
+    AND
+    A.iso_country IN ('GB','FR')
+    AND
+    A.latitude_deg BETWEEN '51' AND '54'  --- List
+
+ORDER BY A.NAME ASC
+
